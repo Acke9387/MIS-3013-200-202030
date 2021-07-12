5c8d93a567fcde06edfa7c7b5b10cfc408278665
Adam Ackerman <adamackerman@outlook.com>
2021-07-12 12:46:30 -0500

    Created starter repository with solutions for the semester

---
+++ Participations/Sum_Of_3_Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sum_Of_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
