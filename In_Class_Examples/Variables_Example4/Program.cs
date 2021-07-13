6b513917246157c9619be227d38f1e0d4748a3f3
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-13 14:09:49 -0500

    Completed Example 4 for Variables

---
+++ In_Class_Examples/Variables_Example4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Variables_Example4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console is a class
+            //Beep is a method that we are calling and providing 2 integer arguments to run
+            Console.Beep(3700, 500);
+        }
+    }
+}
