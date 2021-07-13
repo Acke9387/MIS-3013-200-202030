afd6486822d9ed5aee86c70e96da5af0dc4ddf94
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-13 13:45:47 -0500

    Completed Example 1 for Variables

---
+++ In_Class_Examples/Variables_Example1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+//This a comment
+
+/*  This 
+ *  is
+ *  a
+ *  multi-line
+ *  comment
+ */
+
+namespace Variables_Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my application! What is your name? >>");
+            string name = Console.ReadLine();
+
+            Console.WriteLine(name + ", what is your monthly income? >>");
+            string response = Console.ReadLine();
+            double monthlyIncome = Convert.ToDouble(response); // Convert a string (response) to a double
+
+            Console.WriteLine(name + ", what is your monthly expenses? >>");
+            double monthlyExpenses = Convert.ToDouble(Console.ReadLine());
+
+            double netIncome = monthlyIncome - monthlyExpenses;
+
+            Console.WriteLine($"The difference between your income and expenses is {netIncome.ToString("c3")}.");
+            //Console.WriteLine("The difference between your income and expenses is " + netIncome +".");
+
+        }
+    }
+}
