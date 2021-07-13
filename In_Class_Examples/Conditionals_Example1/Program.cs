fa22334c3383e1753950a17bbb1fc47927150d1b
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-13 15:01:16 -0500

    Completed the Conditionals example 1 problem

---
+++ In_Class_Examples/Conditionals_Example1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Conditionals_Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my application! What is your name? >>");
+            string name = Console.ReadLine(); // this is a local variable
+
+            Console.WriteLine(name + ", what is your monthly income? >>");
+            string response = Console.ReadLine();
+            double monthlyIncome = Convert.ToDouble(response); // Convert a string (response) to a double
+
+            Console.WriteLine(name + ", what is your monthly expenses? >>");
+            double monthlyExpenses = Convert.ToDouble(Console.ReadLine());
+
+            double netIncome = monthlyIncome - monthlyExpenses;
+
+            if (netIncome >= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+            Console.WriteLine($"The difference between your income and expenses is {netIncome.ToString("c3")}.");
+        }
+    }
+}
