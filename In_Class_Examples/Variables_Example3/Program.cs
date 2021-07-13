ee71ae52a07971d0cd082b72807a81c4f49ad282
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-13 14:06:48 -0500

    Completed example 3 for Variables

---
+++ In_Class_Examples/Variables_Example3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variables_Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello, how do you like my colors?");
+
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("How about these colors?");
+        }
+    }
+}
