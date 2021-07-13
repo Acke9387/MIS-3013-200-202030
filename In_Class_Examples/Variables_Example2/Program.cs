fe056c02718d265ef544df2b9ea72d81d46efa1d
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-13 14:00:59 -0500

    Completed example 2 for variables

---
+++ In_Class_Examples/Variables_Example2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variables_Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a lower-bound number. >>");
+            string response = Console.ReadLine();
+            int lower = Convert.ToInt32(response);
+
+            Console.WriteLine("Please enter an upper-bound number. >>");
+            response = Console.ReadLine();
+            int upper = Convert.ToInt32(response);
+
+            Random rand = new Random();
+            int number = rand.Next(lower, upper + 1);
+
+            Console.WriteLine(number.ToString("n0"));
+        }
+    }
+}
