4be17586eb4defb2b58ea22db9679a8145e9ddc3
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-14 13:18:17 -0500

    Completed the Sum of 3 numbers participation

--- Participations/Sum_Of_3_Numbers/Program.cs
+++ Participations/Sum_Of_3_Numbers/Program.cs
@@ -6,7 +6,26 @@ namespace Sum_Of_3_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const double MAGIC_NUMBER = 7.777;
+            double val1, val2;
+            string input;
+            Console.WriteLine("Please enter your first #: >>");
+            input = Console.ReadLine();
+            val1 = Convert.ToDouble(input);
+
+            Console.WriteLine("Please enter your second #: >>");
+            val2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter your third #: >>");
+            double val3 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = (val1 + val2 + val3);
+
+            Console.WriteLine($"The sum of your #'s is {sum.ToString("n3")}");
+                       
+            double secondOutput = sum * MAGIC_NUMBER;
+
+            Console.WriteLine($"{sum.ToString("n3")} * the magic number is {secondOutput.ToString("n3")}");
         }
     }
 }
