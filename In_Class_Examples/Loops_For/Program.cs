067cf8686ac46467f06138031d9cf5433c93f3a6
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-15 14:42:24 -0500

    Completed example over for loop

---
+++ In_Class_Examples/Loops_For/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Loops_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 2; i < 101; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            int sum = 0; //accumulator, just used to accumulate values
+
+            for (int i = 1; i <= 100; i++)
+            {
+                sum = sum + i;
+                //sum += i;
+            }
+
+            Console.WriteLine($"The summation from 1 to 100 is {sum.ToString("N0")}");
+
+        }
+    }
+}
