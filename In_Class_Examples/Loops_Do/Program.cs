caab797aa0334d54fb9a64822eae9822aa7cfa6d
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-15 14:25:29 -0500

    Completed the Loops example for While and started the Do example

---
+++ In_Class_Examples/Loops_Do/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Loops_Do
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0; // lots of times you see this as i
+            double sum = 0; // Referred to as an accumulator
+            string answer;
+            do
+            {
+                Console.WriteLine("Enter your homework grade >>");
+                double grade = Convert.ToDouble(Console.ReadLine());
+
+                //sum = sum + grade;
+                sum += grade;
+
+                //counter = counter + 1;
+                //counter += 1;
+                counter++;
+
+                Console.WriteLine("Do you have another grade to enter? yes or no >>");
+                answer = Console.ReadLine();
+            } while (answer == "yes");
+
+
+        }
+    }
+}
---
+++ In_Class_Examples/Loops_While/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Loops_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your favorite #? >>");
+            string input = Console.ReadLine();
+            int favoriteNumber;
+            bool isValidNumber = int.TryParse(input, out favoriteNumber);
+
+            //while (isValidNumber == false)
+            //{
+            //    Console.WriteLine($"{input} was not a valid integer.  Please input a number >>");
+            //    input = Console.ReadLine();
+            //    isValidNumber = int.TryParse(input, out favoriteNumber);
+            //}
+
+            while (int.TryParse(input, out favoriteNumber) == false)
+            {
+                Console.WriteLine($"{input} was not a valid integer.  Please input a number >>");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Thank you for telling us your favorite number.");
+        }
+    }
+}
