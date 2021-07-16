b5690debbbdd5e69a8617cdbd69f2b96da6e3d52
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-16 13:55:05 -0500

    Completed Example 3 & 4 for Loops

---
+++ In_Class_Examples/Loops_Example3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Loops_Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 900; i++)
+            {
+                if (    i % 3 == 0 
+                   &&   i % 5 == 0)
+                {
+                    Console.WriteLine("Fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+
+            }
+
+
+        }
+    }
+}
---
+++ In_Class_Examples/Loops_Example4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Loops_Example4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //                 0123456790
+            string sentence = "The quick brown fox jumps over the lazy dog";
+
+            //                  01234
+            //string sentence2 = "Hello";
+            //Console.WriteLine(sentence2.Length); // Output: 5
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] != ' ')
+                {
+                    Console.WriteLine(sentence[i]);
+                }
+            }
+
+        }
+    }
+}
