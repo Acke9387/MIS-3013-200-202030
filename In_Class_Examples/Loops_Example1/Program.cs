e7321dda3041c630ac2771edb1f46dd138f257d1
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-16 13:37:24 -0500

    Completed Loops Example 1 & 2

---
+++ In_Class_Examples/Loops_Example1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Loops_Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What would you like to count by? >>");
+            int whatToCountBy = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What number would you like to count up to? >>");
+            int upperBound = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n");
+            /*Special characters:
+             * /n = hitting enter
+             * /t = tab
+             */
+
+            for (int i = whatToCountBy; i <= upperBound; i = i + whatToCountBy)
+            {
+                if (i + whatToCountBy <= upperBound)
+                {
+                    Console.Write(i + ", ");
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+            }
+
+            int counter = whatToCountBy;
+            Console.WriteLine("\nNow the while Loop way ... ");
+            bool isFirstTimeThru = true;
+            while (counter <= upperBound)
+            {
+                if (isFirstTimeThru == true)
+                {
+                    Console.Write(counter);
+                    isFirstTimeThru = false;
+                }
+                else
+                {
+                    Console.Write(", " + counter );
+                }
+
+                counter = counter + whatToCountBy;
+            }
+
+        }
+    }
+}
---
+++ In_Class_Examples/Loops_Example2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Loops_Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What # do you want to count down from? >>");
+            int startingPoint;
+
+            if (int.TryParse(Console.ReadLine(), out startingPoint) == false)
+            {
+                Console.WriteLine("Sorry, that is invalid.  Goodbye.");
+                Environment.Exit(-50);
+            }
+
+            for (int i = startingPoint; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
