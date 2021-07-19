dbe881d0f3c596fe4aea70a93f22bb4ab39a52db
Adam Ackerman <adamackerman@outlook.com>
2021-07-19 13:26:58 -0500

    Completed the coin_toss participation

---
+++ Participations/Coin_Toss/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Coin_Toss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string DEVELOPER_NAME = "Instructor Ackerman";
+
+            Random rand = new Random();
+            int randomNbr = rand.Next(1, 3);
+            const int HEADS = 1;
+            const int TAILS = 2;
+
+            // Used for testing purposes so I could make sure to get a wrong/right answer
+            //Console.WriteLine(randomNbr);
+
+            Console.WriteLine("Heads or Tails? >>");
+            string answer = Console.ReadLine();
+            int answerAsInt = -1;
+
+            Console.WriteLine();
+            if (answer == "Heads")
+            {
+                answerAsInt = HEADS;
+            }
+            else if (answer == "Tails")
+            {
+                answerAsInt = TAILS;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Bye.");
+                Environment.Exit(-1);
+                //answerAsInt = -1;
+            }
+
+            if (randomNbr == HEADS)
+            {
+                Console.WriteLine("Heads");
+            }
+            else
+            {
+                Console.WriteLine("Tails");
+            }
+
+            if (randomNbr == answerAsInt)
+            {
+                Console.WriteLine("Congratulations, you guessed correct!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you guessed incorrect :(");
+            }
+
+            Console.WriteLine($"\nDeveloped by: {DEVELOPER_NAME}");
+        }
+    }
+}
