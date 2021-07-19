541b4efe0c7c828efaefb9048c16b1396dc4264f
Adam Ackerman <adamackerman@outlook.com>
2021-07-19 14:14:30 -0500

    Started an in class example for string manipulation

---
+++ In_Class_Examples/Strings_Example1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Strings_Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //                 0123456790
+            string sentence = "The quick brown fox jumps over the lazy dog";
+
+            Console.WriteLine($"There are {sentence.Length.ToString("n0")} characters in the sentence.");
+
+            Console.WriteLine();
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == ' ')
+                {
+                    Console.WriteLine();
+                    continue;
+                }
+                
+                Console.Write(sentence[i]);
+            }
+
+            Console.WriteLine("Which letter do you want to know the count of? >>");
+            string letter = Console.ReadLine();
+
+
+
+        }
+    }
+}
