63fce88e60dd840dbfa61eeac398a981caf5c676
Adam Ackerman <adamackerman@outlook.com>
2021-07-21 13:24:40 -0500

    Solved the first part of problem 1

---
+++ Participations/Guess_A_Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Guess_A_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the minimum value >>");
+            string input = Console.ReadLine();
+
+            int min = Convert.ToInt32(input);
+
+            Console.WriteLine("Please enter the maximum value >>");
+            input = Console.ReadLine();
+            int max = Convert.ToInt32(input);
+
+            Random rand = new Random();
+            int rando = rand.Next(min, max + 1);
+
+            Console.WriteLine(rando);
+            Console.WriteLine();
+            int guess;
+
+            do
+            {
+                Console.WriteLine("What is your guess? >>");
+                input = Console.ReadLine();
+                guess = Convert.ToInt32(input);
+
+
+            } while (guess != rando);
+
+            Console.WriteLine("Congratulations, you guessed correctly!");
+
+        }
+    }
+}
