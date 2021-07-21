3a6b49fd99adec4df9a7b1465a6579b1f0856850
Adam Ackerman <adamackerman@outlook.com>
2021-07-21 14:13:41 -0500

    Completed an in class example going over arrays and parallel arrays

---
+++ In_Class_Examples/Collections_Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Collections_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstNames = new string[3];
+            // Update the items in the collection to have values
+            firstNames[0] = "Darby";
+            firstNames[1] = "Shelby";
+            firstNames[2] = "Tim";
+
+            //                        0           1          2
+            string[] lastNames = { "Shepard", "Williams", "Davis" };
+
+            Console.WriteLine($"We have {firstNames.Length} first names");
+            Console.WriteLine($"We have {lastNames.Length} last names");
+
+            for (int i = 0; i < firstNames.Length; i++)
+            {
+                string firstName = firstNames[i];
+                string lastName = lastNames[i];
+                Console.WriteLine($"{lastName}, {firstName}");
+            }
+
+            //foreach (string firstName in firstNames)
+            //{
+            //    Console.WriteLine(firstName);
+            //}
+
+        }
+    }
+}
