57dfef96c15f966670095c830621dd0eec7804e1
Adam Ackerman <adamackerman@outlook.com>
2021-07-22 13:15:17 -0500

    Completed a collections example to find the minimum

---
+++ In_Class_Examples/Collections_Example1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections_Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            List<double> numbers = new List<double>();
+            
+            do
+            {
+                Console.WriteLine("Please enter a value >>");
+                input = Console.ReadLine();
+                double number;
+
+                while (double.TryParse(input, out number) == false)
+                {
+                    Console.WriteLine($"{input} is not a valid number. Please enter a valid number >>");
+                    input = Console.ReadLine();
+                }
+
+                numbers.Add(number);
+
+                Console.WriteLine("Do you want to enter another number? yes or no >>");
+                input = Console.ReadLine(); 
+            } while (input.ToLower()[0] == 'y');
+
+            numbers.Add(1);
+            numbers.Add(100);
+            numbers.Add(15);
+
+            double min = numbers[0];
+
+            foreach (double value in numbers)
+            {
+                if (value < min)
+                {
+                    min = value;
+                }
+            }
+
+            Console.WriteLine($"The lowest number in your data set is {min.ToString("N2")}");
+        }
+    }
+}
