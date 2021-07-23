8d410ae63a1fee8ae57f43d57123e52f70c1a994
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-23 13:01:45 -0500

    checking in collections_example1

--- In_Class_Examples/Collections_Example1/Program.cs
+++ In_Class_Examples/Collections_Example1/Program.cs
@@ -1,48 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Collections_Example1
 {
     class Program
     {
-        static void Main(string[] args)
+          static void Main(string[] args)
         {
-            string input;
-            List<double> numbers = new List<double>();
-            
-            do
+            string name1 = "Jenn";
+            char[] name1AsArray = new char[4];
+            name1AsArray[0] = 'J';
+            name1AsArray[1] = 'e';
+            name1AsArray[2] = 'n';
+            name1AsArray[3] = 'n';
+
+            string names = "Scooby Christina JENN clarke";
+            string[] seperatedNames = names.Split(' ');
+            // seperatedNames[0] = "Scooby";
+            // seperatedNames[1] = "Christina";
+            // seperatedNames[2] = "JENN";
+            // seperatedNames[3] = "clarke";
+
+            for (int i = 0; i < seperatedNames.Length; i++)
             {
-                Console.WriteLine("Please enter a value >>");
-                input = Console.ReadLine();
-                double number;
-
-                while (double.TryParse(input, out number) == false)
-                {
-                    Console.WriteLine($"{input} is not a valid number. Please enter a valid number >>");
-                    input = Console.ReadLine();
-                }
-
-                numbers.Add(number);
-
-                Console.WriteLine("Do you want to enter another number? yes or no >>");
-                input = Console.ReadLine(); 
-            } while (input.ToLower()[0] == 'y');
-
-            numbers.Add(1);
-            numbers.Add(100);
-            numbers.Add(15);
-
-            double min = numbers[0];
-
-            foreach (double value in numbers)
-            {
-                if (value < min)
-                {
-                    min = value;
-                }
+                string name = seperatedNames[i];
+                name = name.ToLower();
+                name = name.ToUpper()[0] + name.Substring(1);
+                Console.WriteLine(name);
             }
 
-            Console.WriteLine($"The lowest number in your data set is {min.ToString("N2")}");
         }
     }
 }
