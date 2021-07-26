36311712b560321a16d46f19a506abc87edd2357
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-26 14:03:31 -0500

    started the Functions_Calculator participation

---
+++ Participations/Functions_Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Functions_Calculator
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            DeveloperInformation("Instructor Ackerman", "MIS 3013", "7/26/21");
+
+
+        }
+
+        static double Add(double val1, double val2)
+        {
+            //double sum;
+            //sum = val1 + val2;
+            //return sum;
+            return val1 + val2;
+        }
+
+        static void DeveloperInformation(string name, string course, string date)
+        {
+            Console.WriteLine($"{name} wrote this application for {course} on {date}.");
+        }
+
+
+    }
+}
