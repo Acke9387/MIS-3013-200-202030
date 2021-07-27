fa1b2a1d7f9776bc070485e95898910e76ebdda3
Adam Ackerman <adamackerman@outlook.com>
2021-07-27 13:27:43 -0500

    Finished all types of collections part 2

--- Participations/All_Types_of_Collections_Pt2/Program.cs
+++ Participations/All_Types_of_Collections_Pt2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace All_Types_of_Collections_Pt2
 {
@@ -6,7 +7,30 @@ namespace All_Types_of_Collections_Pt2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<double> grades = new List<double>();
+
+            do
+            {
+                Console.WriteLine("Please enter your grade >>");
+                double grade = Convert.ToDouble(Console.ReadLine());
+                grades.Add(grade);
+
+                Console.WriteLine("Do you have another grade to enter? yes or no >>");
+
+            } while (Console.ReadLine().ToLower() != "no");
+
+            double sum = 0;
+
+            foreach (double grade in grades)
+            {
+                sum += grade;
+
+            }
+            
+            double average = sum / grades.Count;
+            
+            Console.WriteLine($"Your average is {average.ToString("P")}");
+
         }
     }
 }
