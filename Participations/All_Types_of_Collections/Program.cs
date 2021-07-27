a1c875171af71e11d62ee88e6dc5ccc5d87eb8e0
Adam Ackerman <adamackerman@outlook.com>
2021-07-27 13:18:57 -0500

    Completed pt1 of all types of collections

---
+++ Participations/All_Types_of_Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace All_Types_of_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] subjects = new string[3];
+            int[] crns = new int[3];
+
+            for (int i = 0; i < crns.Length; i++)
+            {
+                Console.WriteLine("Please enter a subject >>");
+                string subject = Console.ReadLine();
+                subjects[i] = subject;
+
+                Console.WriteLine("Please enter the course number >>");
+                crns[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            for (int i = 0; i < subjects.Length; i++)
+            {
+                Console.WriteLine($"{subjects[i]}{crns[i]}");
+            }
+
+
+        }
+    }
+}
---
+++ Participations/All_Types_of_Collections_Pt2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace All_Types_of_Collections_Pt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
