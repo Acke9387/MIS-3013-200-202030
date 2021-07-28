9785b5fb9d060a46121d9dc379ba86d2bf91613b
Adam Ackerman <adamackerman@outlook.com>
2021-07-28 13:31:21 -0500

    Finished example converting fizz buzz to a function

---
+++ In_Class_Examples/Functions_Example3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Functions_Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int ranNumber = rand.Next(1, 101);
+            string result = FizzBuzzResult(ranNumber);
+            Console.WriteLine($"{ranNumber} is {result}");
+
+            Console.WriteLine("".PadLeft(20, '-'));
+            for (int i = 1; i < 100; i++)
+            {
+                result = FizzBuzzResult(i);
+                Console.WriteLine(result);
+            }
+
+        }
+
+        static string FizzBuzzResult(int number)
+        {
+            string result;
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                result = "Fizzbuzz";
+            }
+            else if (number % 3 == 0)
+            {
+                result = "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                result = "Buzz";
+            }
+            else
+            {
+                result = number.ToString("n0");
+            }
+
+            return result;
+        }
+
+    }
+}
