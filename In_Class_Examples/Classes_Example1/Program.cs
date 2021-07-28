3b27140227fc790ed8fa9403e1adff5bc73e42d3
Adam Ackerman <adamackerman@outlook.com>
2021-07-28 14:43:16 -0500

    Completed the REctangle class for the in class participation

---
+++ In_Class_Examples/Classes_Example1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Classes_Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+
+            // Example code used for explaining get/set
+            //r1.Width = 5;
+            //r1.SetWidth(5);
+            //double val = r1.Width;
+            //val = r1.GetWidth();
+
+            r1.Length = 10;
+            r1.Width = 5;
+
+            Rectangle r2 = new Rectangle();
+            r2.Length = 5;
+            r2.Width = 5;
+
+            double a = r1.CalculateArea();
+            double p = r1.CalculatePerimeter();
+
+            Console.WriteLine($"The area of r1 is {a}");
+            Console.WriteLine($"The perimeter of r1 is {p}");
+
+            a = r2.CalculateArea();
+            p = r2.CalculatePerimeter();
+
+            Console.WriteLine($"The area of r2 is {a}");
+            Console.WriteLine($"The perimeter of r2 is {p}");
+
+        }
+    }
+}
---
+++ In_Class_Examples/Classes_Example1/Rectangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Example1
+{
+    public class Rectangle
+    {
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+        //private double _Width;
+        //public double GetWidth()
+        //{
+        //    return _Width;
+        //}
+        //public void SetWidth(double w)
+        //{
+        //    _Width = w;
+        //}
+
+        /// <summary>
+        /// Default/Empty Constructor
+        /// </summary>
+        public Rectangle()
+        {
+            Length = 0;
+            Width = 0;
+        }
+
+        /// <summary>
+        /// Calculates the Area of THIS Rectangle
+        /// </summary>
+        /// <returns>The area of THIS rectangle</returns>
+        public double CalculateArea()
+        {
+            double area;
+
+            area = Length * Width;
+
+            return area;
+        }
+
+        /// <summary>
+        /// Calculates the Perimeter of THIS rectangle
+        /// </summary>
+        /// <returns>The perimeter of THIS rectangle</returns>
+        public double CalculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+        
+    }
+}
