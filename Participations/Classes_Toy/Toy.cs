bd9e660f89bf8d347275ac350891071bdb9d9097
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-29 14:00:25 -0500

    Started the Toy participation

---
+++ Participations/Classes_Toy/Toy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Toy
+{
+    public class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        
+        private string Notes;
+
+        public Toy()
+        {
+            // Fill this out
+        }
+
+        public string GetAisle()
+        {
+            // Implement this based on description
+        }
+
+    }
+}
