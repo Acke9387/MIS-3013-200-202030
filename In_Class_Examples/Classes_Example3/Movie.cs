6131b428eedbf086e47e1f43230df46225b7a084
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-29 13:53:35 -0500

    Did another example over classes pertaining to a Movie

---
+++ In_Class_Examples/Classes_Example3/Movie.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Example3
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public List<string> Genres { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        /// <summary>
+        /// Default/Empty constructor for a Movie
+        /// </summary>
+        public Movie()
+        {
+            Title = string.Empty;// "";
+            ReleaseYear = 0;
+            Genres = new List<string>();
+        }
+
+        /// <summary>
+        /// Overloaded constructor that takes the title and year of the movie as arguments
+        /// </summary>
+        /// <param name="title">Movie Title</param>
+        /// <param name="year">Release year of the movie</param>
+        public Movie(string title, int year)
+        {
+            Title = title;
+            ReleaseYear = year;
+            Genres = new List<string>();
+        }
+
+        /// <summary>
+        /// Overloaded constructor that takes the title, year and genres of the movie
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="year"></param>
+        /// <param name="genres"></param>
+        public Movie(string title, int year, string[] genres)
+        {
+            Title = title;
+            ReleaseYear = year;
+            Genres = new List<string>();
+
+            foreach (string genre in genres)
+            {
+                Genres.Add(genre);
+            }
+
+        }
+
+        /// <summary>
+        /// Determines how many years old the movie is.
+        /// </summary>
+        /// <returns>The number of years since the release of the movie</returns>
+        public int Age()
+        {
+            DateTime today = DateTime.Now;
+
+            int age = today.Year - ReleaseYear;
+
+            return age;
+        }
+
+
+        public override string ToString()
+        {
+            string output = "";
+            int age = Age();
+
+            switch (age)
+            {
+                case 0:
+                    output = $"{Title} was released in {ReleaseYear} and has the genres of : {string.Join(", ", Genres)}.";
+                    break;
+                case 1:
+                    output = $"{Title} was released in {ReleaseYear} (which makes it {age} year old) and has the genres of : {string.Join(", ", Genres)}.";
+                    break;
+                default:
+                    output = $"{Title} was released in {ReleaseYear} (which makes it {age} years old) and has the genres of : {string.Join(", ", Genres)}.";
+                    break;
+            }
+
+            
+
+            return output;           
+        }
+
+    }
+}
---
+++ In_Class_Examples/Classes_Example3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes_Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie();
+            movie1.Title = "Jungle Cruise";
+            movie1.ReleaseYear = 2021;
+            movie1.Genres.Add("Action");
+            movie1.Genres.Add("Adventure");
+            movie1.Genres.Add("Comedy");
+
+            Movie movie2 = new Movie("Free Guy", 2021);
+            movie2.Genres.Add("Comedy");
+            movie2.Genres.Add("Action");
+            movie2.Genres.Add("Sci-Fi");
+
+            string[] genres = { "Action", "Drama", "Adventure" };
+
+            Movie movie3 = new Movie("Avengers: End Game", 2019, genres);
+
+            Console.WriteLine(movie1.ToString()); //better to get used to not adding the .ToString()
+            Console.WriteLine(movie2);
+            Console.WriteLine(movie3);
+
+            int age = Age(movie1); // different way to solve the problem, but is not the 'correct' way
+        }
+
+        static int Age(Movie m)
+        {
+            DateTime today = DateTime.Now;
+
+            int age = today.Year - m.ReleaseYear;
+
+            return age;
+        }
+    }
+}
