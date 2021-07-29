4a597d3a092a0b2c5515280383fb8901daf8feba
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-29 14:06:38 -0500

    Completed the participation over Functions - Double Input Validation

---
+++ Participations/Functions_DoubleInputValidation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Functions_DoubleInputValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input = GetDoubleInputFromUser("Please enter your value >>");
+
+            Console.WriteLine($"{input.ToString("N2")} * 5 = {(input*5).ToString("N2")}");
+
+        }
+
+        static double GetDoubleInputFromUser(string message)
+        {
+            Console.WriteLine(message);
+            string response = Console.ReadLine();
+            double value;
+            while (double.TryParse(response, out value) == false)
+            {
+                Console.WriteLine("Invalid input.  Please input a valid number.>>");
+                response = Console.ReadLine();
+            }
+
+            return value;
+        }
+    }
+}
