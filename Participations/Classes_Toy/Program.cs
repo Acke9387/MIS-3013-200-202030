9676e5306ee90c9718ca2848d09ca74f68714fbc
Ackerman, Adam P <adamackrmn@gmail.com>
2021-07-30 13:18:32 -0500

    Completed the Classes - Toy participation

--- Participations/Classes_Toy/Program.cs
+++ Participations/Classes_Toy/Program.cs
@@ -6,7 +6,25 @@ namespace Classes_Toy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Toy toy1 = new Toy();
+            toy1.Manufacturer = "Hasbro";
+            toy1.Name = "Twister";
+            toy1.Price = 14.99;
+            toy1.AddNote("Crazy fun for the whole family!");
+            toy1.AddNote("Ages 5+");
+
+            Toy toy2 = new Toy("Hours of fun!");
+            toy2.Manufacturer = "Nintendo";
+            toy2.Name = "Switch";
+            toy2.Price = 244.99;
+
+            // Don't need to do this, since we moved the logic to our Override ToString() method inside the class
+            //Console.WriteLine($"{toy1.Name} is made by {toy1.Manufacturer} and costs {toy1.Price}.  Notes: {toy1.GetNotes()}.");
+            //Console.WriteLine($"{toy2.Name} is made by {toy2.Manufacturer} and costs {toy2.Price}.  Notes: {toy2.GetNotes()}.");
+
+            Console.WriteLine(toy1);
+            Console.WriteLine(toy2);
+
         }
     }
 }
--- Participations/Classes_Toy/Toy.cs
+++ Participations/Classes_Toy/Toy.cs
@@ -9,18 +9,51 @@ namespace Classes_Toy
         public string Manufacturer { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
-        
+
         private string Notes;
 
         public Toy()
         {
-            // Fill this out
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Notes = string.Empty;
+        }
+
+        public Toy(string note)
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Notes = note;
+        }
+
+        public void AddNote(string note)
+        {
+            Notes += "\n" + note;
+        }
+
+        public string GetNotes()
+        {
+            return Notes;
         }
 
         public string GetAisle()
         {
-            // Implement this based on description
+            Random rand = new Random();
+            int number = rand.Next(1, 25);
+
+            string aisle = Manufacturer.ToUpper().Substring(0, 1) + number;
+            aisle = $"{Manufacturer.ToUpper()[0]}{number}";
+            aisle = Manufacturer.ToUpper()[0].ToString() + number;
+            aisle = "" + Manufacturer.ToUpper()[0] + number;
+
+            return aisle;
         }
 
+        public override string ToString()
+        {
+            return $"{Name} is made by {Manufacturer} and costs {Price}.  Notes: {Notes}.";
+        }
     }
 }
