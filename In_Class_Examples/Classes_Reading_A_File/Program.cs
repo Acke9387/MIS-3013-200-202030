7d6ab89e6229bd8772b14cf70ddfde5d61c98cce
Ackerman, Adam P <adamackrmn@gmail.com>
2021-08-02 13:12:11 -0500

    Started in class example over classes reading in a file

---
+++ In_Class_Examples/Classes_Reading_A_File/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Classes_Reading_A_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+        }
+    }
+}
---
+++ In_Class_Examples/Classes_Reading_A_File/Sale.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Reading_A_File
+{
+    public class Sale
+    {
+        public int OrderNumber { get; set; }
+        public int QuantityOrdered { get; set; }
+        public double PriceEach { get; set; }
+        public int OrderLineNumber { get; set; }
+        public double Sales { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Status { get; set; }
+        public int Quarter_ID { get; set; }
+        public int Month_ID { get; set; }
+        public int Year_ID { get; set; }
+        public string ProductLine { get; set; }
+        public double MSRP { get; set; }
+        public string ProductCode { get; set; }
+        public string CustomerName { get; set; }
+        public string Phone { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Territory { get; set; }
+        public string ContactLastName { get; set; }
+        public string ContactFirstName { get; set; }
+        public string DealSize { get; set; }
+
+
+
+
+    }
+}
