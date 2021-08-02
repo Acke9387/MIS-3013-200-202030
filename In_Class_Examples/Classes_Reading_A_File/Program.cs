94fa631969302e3ad4467202f29e97229c207b44
Ackerman, Adam P <adamackrmn@gmail.com>
2021-08-02 13:51:35 -0500

    Checking in code to output sales by country

--- In_Class_Examples/Classes_Reading_A_File/Program.cs
+++ In_Class_Examples/Classes_Reading_A_File/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Classes_Reading_A_File
 {
@@ -8,7 +10,42 @@ namespace Classes_Reading_A_File
         {
 
 
-            
+            string[] lines = File.ReadAllLines("sales_data_sample.csv");
+
+            List<Sale> sales = new List<Sale>();
+            for (int i = 1; i < lines.Length; i++)
+            {
+                Sale s = new Sale(lines[i]);
+               
+                // If we were doing the setting of values in our Main method vs our constructor
+                //string[] pieces = new string[3];
+                //s.OrderNumber = Convert.ToInt32(pieces[0]);
+                //s.QuantityOrdered = Convert.ToInt32(pieces[1]);
+                //s.PriceEach = Convert.ToDouble(pieces[2]);
+                sales.Add(s);
+            }
+
+            OutputAllSalesFromCountry(sales, "USA");
+            OutputAllSalesFromCountry(sales, "France");
+            OutputAllSalesWithAProfit(sales);
+
+        }
+
+        private static void OutputAllSalesWithAProfit(List<Sale> sales)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void OutputAllSalesFromCountry(List<Sale> sales, string country)
+        {
+            Console.WriteLine("".PadLeft(10,'*') + $"{country} Sales" + "".PadRight(10, '*'));
+            foreach (Sale sale in sales)
+            {
+                if (sale.Country.ToUpper() == country.ToUpper())
+                {
+                    Console.WriteLine(sale) ;
+                }
+            }
         }
     }
 }
--- In_Class_Examples/Classes_Reading_A_File/Sale.cs
+++ In_Class_Examples/Classes_Reading_A_File/Sale.cs
@@ -61,5 +61,56 @@ namespace Classes_Reading_A_File
             DealSize            = string.Empty;
         }
 
+        public Sale(string lineOfFile)
+        {
+            string[] pieces = lineOfFile.Split(',');
+            //     0	         1	          2	           3	      4	     5	         6	    7	     8	      9	     10	       11	      12	     13	           14	     15	            16	      17	   18	   19	     20	      21	          22	           23	          24
+            //ORDERNUMBER QUANTITYORDERED PRICEEACH ORDERLINENUMBER SALES ORDERDATE   STATUS QTR_ID  MONTH_ID YEAR_ID PRODUCTLINE MSRP    PRODUCTCODE CUSTOMERNAME    PHONE ADDRESSLINE1    ADDRESSLINE2 CITY    STATE POSTALCODE  COUNTRY TERRITORY   CONTACTLASTNAME 
+
+            OrderNumber =     Convert.ToInt32(pieces[0]);
+            QuantityOrdered = Convert.ToInt32(pieces[1]);
+            PriceEach = Convert.ToDouble(pieces[2]);
+            OrderLineNumber = Convert.ToInt32(pieces[3]);
+            Sales = Convert.ToDouble(pieces[4]);
+            OrderDate = Convert.ToDateTime(pieces[5]);
+            Status = pieces[6];
+            Quarter_ID = Convert.ToInt32(pieces[7]);
+            Month_ID =   Convert.ToInt32(pieces[8]);
+            Year_ID =    Convert.ToInt32(pieces[9]);
+            ProductLine = pieces[10];
+            MSRP = Convert.ToDouble(pieces[11]);
+            ProductCode = pieces[12];
+            CustomerName = pieces[13];
+            Phone = pieces[14];
+            AddressLine1 = pieces[15];
+            AddressLine2 = pieces[16];
+            City = pieces[17];
+            State = pieces[18];
+            PostalCode = pieces[19];
+            Country =   pieces[20];
+            Territory = pieces[21];
+            ContactLastName = pieces[22];
+            ContactFirstName = pieces[23];
+            DealSize = pieces[24];
+        }
+
+        /// <summary>
+        /// Returns the profit of the sale
+        /// </summary>
+        /// <returns>
+        /// <c>Positive if you made money</c>
+        /// <c>Negative if you made money</c>
+        /// </returns>
+        public double Profit()
+        {
+            return (MSRP * QuantityOrdered) - Sales;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{OrderNumber} {OrderDate.ToShortDateString()} - {CustomerName}";
+        }
+
     }
 }
