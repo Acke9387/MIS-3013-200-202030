2ac1f8dae087003b87789e35bb22db2bb942a496
Ackerman, Adam P <adamackrmn@gmail.com>
2021-08-04 13:32:39 -0500

    Finished the Cereal participation

---
+++ Participations/Classes_Cereal/Cereal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Cereal
+{
+    public class Cereal
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double Cups { get; set; }
+
+        public Cereal()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Calories = 0;
+            Cups = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} is made by {Manufacturer}.  There are {Calories} per {Cups} cup(s)";
+        }
+    }
+}
---
+++ Participations/Classes_Cereal/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Classes_Cereal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 0        1          2       3
+            //name|manufacturer|calories|cups
+            //100 % Bran | Nabisco | 70 | 0.33
+
+            string[] lines = File.ReadAllLines("Cereal_Data.txt");
+            List<Cereal> cereals = new List<Cereal>();
+
+            // Process all of the information from the file and store it in a collection
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] cerealInformation = lines[i].Split('|');
+
+                Cereal temp = new Cereal();
+                temp.Name = cerealInformation[0];
+                temp.Manufacturer = cerealInformation[1];
+                temp.Calories = Convert.ToDouble(cerealInformation[2]);
+                temp.Cups = Convert.ToDouble(cerealInformation[3]);
+
+                cereals.Add(temp);
+            }
+
+            OutputCerealsWithServingSize(cereals, 1);
+            OutputCerealsWithCaloriesLessThan(cereals, 100);
+        }
+
+        private static void OutputCerealsWithServingSize(List<Cereal> cereals, double lowerBoundServingSize)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("".PadLeft(15, '*') + $" Serving Size >= {lowerBoundServingSize} cups " + "".PadLeft(15, '*'));
+            foreach (Cereal cereal in cereals)
+            {
+                if (cereal.Cups >= lowerBoundServingSize)
+                {
+                    Console.WriteLine(cereal);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        private static void OutputCerealsWithCaloriesLessThan(List<Cereal> cereals, double lowerBoundCalories)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("".PadLeft(15, '*') + $" Cereals w/ <= {lowerBoundCalories} calories " + "".PadLeft(15, '*'));
+            foreach (Cereal cereal in cereals)
+            {
+                if (cereal.Calories <= lowerBoundCalories)
+                {
+                    Console.WriteLine(cereal);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
