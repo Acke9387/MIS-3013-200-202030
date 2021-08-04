022724dcc608fe023427319e319c36a5c939485e
Ackerman, Adam P <adamackrmn@gmail.com>
2021-08-04 14:05:42 -0500

    Did an in class example dealing with a database

---
+++ In_Class_Examples/Data_SQLClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Data_SQLClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=tcp:s13.winhost.com;Initial Catalog=DB_128040_practice;User ID=student;Password=AdamTheGreat2020;Integrated Security=False;";
+            List<Student> students = new List<Student>();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                string query = "SELECT StudentID, FirstName, LastName, FavoriteColor " +
+                               "FROM Student";
+
+                using (SqlCommand command = new SqlCommand(query, con))
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        int id;
+                        string first, last, favColor;
+
+                        //id = (int)reader["StudentID"];
+                        id = Convert.ToInt32(reader["StudentID"]);
+                        first = Convert.ToString(reader[1]);
+                        last = reader[2].ToString();
+                        favColor = reader[3].ToString();
+
+                        Student s = new Student();
+                        s.StudentID = id;
+                        s.FirstName = first;
+                        s.LastName = last;
+                        s.FavoriteColor = favColor;
+                        students.Add(s);
+                    }
+
+                }
+
+            }
+
+            foreach (Student student in students)
+            {
+                if (!student.FavoriteColor.ToLower().Contains("blue"))
+                {
+                    Console.WriteLine(student);
+                }
+            }
+
+        }
+    }
+}
---
+++ In_Class_Examples/Data_SQLClient/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_SQLClient
+{
+    public class Student
+    {
+
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FavoriteColor { get; set; }
+
+        public Student()
+        {
+            StudentID = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            FavoriteColor = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName} ({StudentID}) likes {FavoriteColor}";
+        }
+    }
+}
